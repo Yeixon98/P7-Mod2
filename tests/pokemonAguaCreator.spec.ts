8ac678f03e80064697aaf0b89c6db255a40e1d4f
Yeixon Morales Gonzalez <alu0101133324@ull.edu.es>
2022-04-07 11:47:12 +0200

    Add More Test

--- tests/pokemonAguaCreator.spec.ts
+++ tests/pokemonAguaCreator.spec.ts
@@ -3,14 +3,38 @@ import { expect } from "chai";
 import { PokemonAguaCreator } from "../src/factory/pokemonAguaCreator";
 import { FactoryPokemon } from "../src/factory/factory";
 
-describe("Class PokemonAguaCreator", () => {
-  const pokemon: PokemonAguaCreator = new PokemonAguaCreator("Vaporeon",12,24,[45,23,12])
-  it("Create PokemonCreator", () => {
-    expect(pokemon).to.exist;
-    expect(pokemon).to.be.an.instanceOf(FactoryPokemon);
+describe("Class PokemonAguaCreator Instance 1", () => {
+  const squirtle: PokemonAguaCreator = new PokemonAguaCreator("Squirtle",24,15,[12,23,12])
+  it("Create Squirtle", () => {
+    expect(squirtle).to.exist;
+    expect(squirtle).to.be.an.instanceOf(FactoryPokemon);
   });
 
   it("Logic Factory", () => {
-    expect(pokemon.logic()).to.eql("Name: Vaporeon, Altura: 12, Peso: 24, Estadisticas: 45-23-12")
+    expect(squirtle.logic()).to.eql("Name: Squirtle, Altura: 24, Peso: 15, Estadisticas: 12-23-12")
+  })
+});
+
+describe("Class PokemonAguaCreator Instance 2", () => {
+  const tentacool: PokemonAguaCreator = new PokemonAguaCreator("Tentacool",14,18,[45,28,12])
+  it("Create Tentacool", () => {
+    expect(tentacool).to.exist;
+    expect(tentacool).to.be.an.instanceOf(FactoryPokemon);
+  });
+
+  it("Logic Factory", () => {
+    expect(tentacool.logic()).to.eql("Name: Tentacool, Altura: 14, Peso: 18, Estadisticas: 45-28-12")
+  })
+});
+
+describe("Class PokemonAguaCreator Instance 3", () => {
+  const seel: PokemonAguaCreator = new PokemonAguaCreator("Seel",12,50,[15,23,9])
+  it("Create Seel", () => {
+    expect(seel).to.exist;
+    expect(seel).to.be.an.instanceOf(FactoryPokemon);
+  });
+
+  it("Logic Factory", () => {
+    expect(seel.logic()).to.eql("Name: Seel, Altura: 12, Peso: 50, Estadisticas: 15-23-9")
   })
 });
--- tests/pokemonTierraCreator.spec.ts
+++ tests/pokemonTierraCreator.spec.ts
@@ -3,14 +3,38 @@ import { expect } from "chai";
 import { PokemonTierraCreator } from "../src/factory/pokemonTierraCreator";
 import { FactoryPokemon } from "../src/factory/factory";
 
-describe("Class PokemonTierraCreator", () => {
-  const pokemon: PokemonTierraCreator = new PokemonTierraCreator("Digglet",12,24,[45,23,12])
-  it("Create PokemonCreator", () => {
-    expect(pokemon).to.exist;
-    expect(pokemon).to.be.an.instanceOf(FactoryPokemon);
+describe("Class PokemonTierraCreator Instance 1", () => {
+  const digglet: PokemonTierraCreator = new PokemonTierraCreator("Digglet",12,15,[10,23,12])
+  it("Create Digglet", () => {
+    expect(digglet).to.exist;
+    expect(digglet).to.be.an.instanceOf(FactoryPokemon);
   });
 
   it("Logic Factory", () => {
-    expect(pokemon.logic()).to.eql("Name: Digglet, Altura: 12, Peso: 24, Estadisticas: 45-23-12")
+    expect(digglet.logic()).to.eql("Name: Digglet, Altura: 12, Peso: 15, Estadisticas: 10-23-12")
+  })
+});
+
+describe("Class PokemonTierraCreator Instance 2", () => {
+  const sandshrew: PokemonTierraCreator = new PokemonTierraCreator("Sandshrew",12,15,[12,23,12])
+  it("Create Sandshrew", () => {
+    expect(sandshrew).to.exist;
+    expect(sandshrew).to.be.an.instanceOf(FactoryPokemon);
+  });
+
+  it("Logic Factory", () => {
+    expect(sandshrew.logic()).to.eql("Name: Sandshrew, Altura: 12, Peso: 15, Estadisticas: 12-23-12")
+  })
+});
+
+describe("Class PokemonTierraCreator Instance 3", () => {
+  const cubone: PokemonTierraCreator = new PokemonTierraCreator("Cubone",12,9,[10,12,12])
+  it("Create Cubone", () => {
+    expect(cubone).to.exist;
+    expect(cubone).to.be.an.instanceOf(FactoryPokemon);
+  });
+
+  it("Logic Factory", () => {
+    expect(cubone.logic()).to.eql("Name: Cubone, Altura: 12, Peso: 9, Estadisticas: 10-12-12")
   })
 });
